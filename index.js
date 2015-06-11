cedc8b113e07fd1615b3a4e8e3c6ef2d6b1d0af8
kaflan <kaf_90@mail.ru>
2015-06-11 13:53:55 +0300

    send

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 /**
  * Created by kaflan on 06.06.15.
  */
-window.addEventListener('load', function () {
+window.addEventListener('load', function load() {
+  'use strict';
   var body = document.querySelector('body');
   var head = document.querySelector('head');
-  //  create el
   var input = document.createElement('input');
   var button = document.createElement('button');
   var div = document.createElement('div');
-  var script = document.createElement('script');
-  // append child
+  var script;
+  var queryDiv;
+  var queryInput;
+  var queryButton;
+  var title;
+  var queryScriptAll;
+  var url;
   body.appendChild(input);
   body.appendChild(button);
   body.appendChild(div);
-  //query selectors
-  var queryDiv = document.querySelector('div');
-  var queryInput = document.querySelector('input');
-  var queryButton = document.querySelector('button');
-  //global function
-  //window or this
-  window.showResult = function (data) {
-    console.log(data);
+  queryDiv = document.querySelector('div');
+  queryInput = document.querySelector('input');
+  queryButton = document.querySelector('button');
+  window.showResult = function Glob(data) {
     queryDiv.innerHTML = data.parse.text['*'];
   };
-  //function
-  var sendRequest = function () {
-    //title
-    var title = queryInput.value;
-    //create script
-    var script = document.createElement('script');
+  // function
+  function sendRequest() {
+    // title
+    title = queryInput.value;
+    // create script
+    script = document.createElement('script');
     head.appendChild(script);
-    var queryScriptAll = document.querySelectorAll('script');
-    //
-    var url = 'http://en.wikipedia.org/w/api.php?action=parse&page=' + title + '&prop=text&section=0&format=json&callback=showResult';
+    queryScriptAll = document.querySelectorAll('script');
+    url = 'http://en.wikipedia.org/w/api.php?action=parse&page=' + title + '&prop=text&section=0&format=json&callback=showResult';
     queryScriptAll[1].src = url;
-    if(queryScriptAll[1].parentNode){
+    if (queryScriptAll[1].parentNode) {
       queryScriptAll[1].parentNode.removeChild(queryScriptAll[1]);
     }
-  };
+  }
+
   // any
   queryDiv.id = 'content';
   queryButton.innerHTML = 'Go';
   // listener
-  queryInput.addEventListener('keyup', function(e){
-    if(e.keyCode !== 13) return;
+  queryInput.addEventListener('keyup', function inputFun(e) {
+    if (e.keyCode !== 13) return;
     sendRequest();
   });
   queryButton.addEventListener('click', sendRequest);
